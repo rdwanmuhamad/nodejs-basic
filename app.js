792f4030652b4ace282689466688c083069ca47e
rdwanmuhamad <mrdwn624@gmail.com>
2023-06-20 15:53:47 +0800

    npm module

--- app.js
+++ app.js
@@ -8,8 +8,16 @@
 
 // module built in
 
-const fs = require("fs");
+// const fs = require("fs");
 
-fs.copyFileSync("text.txt", "text2.txt");
-console.log("text.txt was copied to text2.txt");
+// fs.copyFileSync("text.txt", "text2.txt");
+// console.log("text.txt was copied to text2.txt");
 
+// npm module
+
+const superhero = require("superheroes");
+
+for (let i = 0; i < 10; i++) {
+  const nameHero = superhero.random();
+  console.log(nameHero);
+}
