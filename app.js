727a558cb1b4030bc384b5e8a95ad6269594e050
rdwanmuhamad <mrdwn624@gmail.com>
2023-06-20 15:40:15 +0800

    module built in node js

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-// console.log('Hello World!')
+// console.log('Hello World!');
 
-function tambah(a,b){
-    return a + b
-}
+// function tambah(a,b){
+//     return a + b
+// }
 
-console.log(tambah(10, 5))
+// console.log(tambah(10, 5));
+
+// module built in
+
+const fs = require("fs");
+
+fs.copyFileSync("text.txt", "text2.txt");
+console.log("text.txt was copied to text2.txt");
+
