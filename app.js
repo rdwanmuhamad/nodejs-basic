ff23cf7ad04d454060105c7375ae9f26651071bb
rdwanmuhamad <mrdwn624@gmail.com>
2023-06-21 09:01:44 +0800

    custom module

--- app.js
+++ app.js
@@ -15,9 +15,21 @@
 
 // npm module
 
-const superhero = require("superheroes");
+// const superhero = require("superheroes");
 
-for (let i = 0; i < 10; i++) {
-  const nameHero = superhero.random();
-  console.log(nameHero);
-}
+// for (let i = 0; i < 10; i++) {
+//   const nameHero = superhero.random();
+//   console.log(nameHero);
+// }
+
+
+const op = require("./module");
+
+const moduleTitle = op.title;
+const modulePenjumlahan = op.tambah(10, 5);
+const modulePerkalian = op.kali(10, 5);
+
+
+console.log(moduleTitle);
+console.log(modulePenjumlahan);
+console.log(modulePerkalian);
